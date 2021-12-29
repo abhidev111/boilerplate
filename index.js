1c2a3f53d80276989f8231265d3b4ccd2b4eeaf7
Abhishek_K <abhishekdevadiga8103@gmail.com>
2021-12-29 22:43:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const usersRoute = require('./src/routes/users');
+
+
+dotenv.config();
+//middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+
+app.get("/ping",(req,res)=>{
+    res.send("HOHO Welcome to Socially  !!")
+})
+
+app.use("users/", usersRoute);
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log("Backend server is ready...")
+})
+
+
+// mongoose.connect(process.env.MONGO_URL, (err)=>{
+//     if(!err){
+//         console.log("successfully connected to mongoDB")
+//     }
+//     else{
+//         console.log("Coudn't connect to mongoDB")
+//     }
+// });
+
---
+++ src/routes/users.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+console.log("hello")
+
+router.post("/users/signup",(req,res)=>{
+    console.log("hello inside");
+    res.send("router info");
+})
+
+router.post("/login",(req,res)=>{
+    res.send("router info");
+})
+
+router.post("/users/emailvrify",(req,res)=>{
+    res.send("router info");
+})
+
+router.get("/users/:id",(req,res)=>{
+    res.send("router info");
+})
+
+router.put("/users/:id",(req,res)=>{
+    res.send("router info");
+})
+
+module.exports = router;
